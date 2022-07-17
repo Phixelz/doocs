1860a0eec51f8c1bf69ca6bcf8131b1937faf537
Phillipi Alves <phixelz@gmail.com>
2022-07-17 16:04:57 -0300

    divider created

---
+++ src/components/Divider/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  border-bottom-width: 1px;
+  border-color: ${({ theme }) => theme.colors.secondary_light};
+  margin: 8px 0 8px;
+`;
