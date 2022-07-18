956f5e6a3f6c62e8e05550ea9ec1433c21ea2930
Phillipi Alves <phixelz@gmail.com>
2022-07-17 22:47:07 -0300

    new type created

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,15 +3,15 @@ import { TouchableOpacityProps } from "react-native";
 import { CustomButton, ButtonText, Icon } from "../Button/styles";
 
 interface ButtonProps extends TouchableOpacityProps {
-  type: "default" | "disabled";
+  type: "default" | "disabled" | "link";
   title: string;
 }
 
 export function Button({ type, title, ...rest }: ButtonProps) {
   return (
     <CustomButton type={type} {...rest}>
-      <ButtonText>{title}</ButtonText>
-      <Icon name="arrow-right" />
+      <ButtonText type={type}>{title}</ButtonText>
+      <Icon type={type} name="arrow-right" />
     </CustomButton>
   );
 }
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -2,7 +2,7 @@ import styled, {css} from "styled-components/native";
 import { Feather } from "@expo/vector-icons";
 
 interface ButtonProps {
-  type: "default" | "disabled";
+  type: "default" | "disabled" | "link";
 }
 
 export const CustomButton = styled.TouchableOpacity<ButtonProps>`
@@ -11,21 +11,39 @@ export const CustomButton = styled.TouchableOpacity<ButtonProps>`
   align-items: center;
   justify-content: flex-end;
   border-radius: 8px;
+
+  ${(props) => props.type === 'default' && css`
   background-color: ${({ theme }) => theme.colors.primary};
+ `}
 
  ${(props) => props.type === 'disabled' && css`
+  background-color: ${({ theme }) => theme.colors.primary};
   opacity: 0.5;
+ `}
+
+ ${(props) => props.type === 'link' && css`
+  padding: 0;
  `}
 `;
 
-export const ButtonText = styled.Text`
+export const ButtonText = styled.Text<ButtonProps>`
   font-size: 16px;
   font-family: "Poppins_600SemiBold";
   color: ${({ theme }) => theme.colors.secondary};
+
+  ${(props) => props.type === 'link' && css`
+  font-size: 14px;
+  font-family: "Poppins_500Medium";
+  color: ${({ theme }) => theme.colors.primary};
+ `}
 `;
 
-export const Icon = styled(Feather)`
+export const Icon = styled(Feather)<ButtonProps>`
   font-size: 24px;
   color: ${({ theme }) => theme.colors.secondary};
   margin-left: 8px;
+
+  ${(props) => props.type === 'link' && css`
+  display: none;
+ `}
 `;
